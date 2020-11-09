69bd949f9b7498bf5b5d1e3fb5317f4c75300d86
Victor Lopez <A01336434@itesm.mx>
2020-11-09 12:59:32 -0600

    Merged back and front, solved issues from back

--- src/services/queues/queue.js
+++ src/services/queues/queue.js
@@ -1,22 +1,22 @@
 class Queue {
-    constructor(lambda, mu, s, k, cw, cs) {
-        this.lambda = lambda;
-        this.mu = mu;
-        this.s = s;
-        this.k = k;
-        this.cw = cw;
-        this.cs = cs;
-    }
+  constructor(lambda, mu, s, k, cw, cs) {
+    this.lambda = lambda;
+    this.mu = mu;
+    this.s = s;
+    this.k = k;
+    this.cw = cw;
+    this.cs = cs;
+  }
 
-    lq() { }
+  lq() {}
 
-    rho() {
-        return this.lambda/(this.s*this.mu);
-    }
+  rho() {
+    return this.lambda / (this.s * this.mu);
+  }
 
-    ct() {
-        return this.lq() * this.cw + this.s * this.cs;
-    }
+  ct() {
+    return this.lq() * this.cw + this.s * this.cs;
+  }
 }
 
 export default Queue;
